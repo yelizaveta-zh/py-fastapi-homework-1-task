153fd3ca062e0ddf52c18268c84b0a020e68270e
Lisa <zhyganiuk.elizaveta@gmail.com>
2025-03-13 02:34:39 +0200

    Solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -3,8 +3,58 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db, MovieModel
-
+from schemas import MovieListResponseSchema, MovieDetailResponseSchema, movies
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get(
+    "/movies/",
+    response_model=MovieListResponseSchema
+)
+async def get_movies(
+        db: AsyncSession = Depends(get_db),
+        page: int = Query(1, ge=1),
+        per_page: int = Query(10, ge=1, le=20),
+):
+    result = await db.execute(select(MovieModel))
+    total_items = len(result.scalars().all())
+    total_pages = (total_items + per_page - 1) // per_page
+    if total_items == 0 or page > total_pages:
+        raise HTTPException(status_code=404, detail="No movies found.")
+    if page < 1 or per_page < 10 or per_page > 20:
+        raise HTTPException(
+            status_code=422,
+            detail="ensure this value is greater than or equal to 1"
+        )
+    prev_page = None\
+        if page <= 1\
+        else f"/api/v1/theater/movies/?page={page-1}&per_page={per_page}"
+    next_page = None\
+        if page >= total_pages\
+        else f"/api/v1/theater/movies/?page={page+1}&per_page={per_page}"
+
+    return MovieListResponseSchema(
+        movies=result,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total_items,
+    )
+
+
+@router.get(
+    "/movies/{movie_id}/",
+    response_model=MovieDetailResponseSchema
+)
+async def get_movie_by_id(
+        movie_id: int,
+        db: AsyncSession = Depends(get_db)
+):
+    movie = await db.get(MovieModel, movie_id)
+    if not movie:
+        raise HTTPException(
+            status_code=404,
+            detail="Movie with the given ID was not found."
+        )
+    return MovieDetailResponseSchema(**movie.__dict__)
--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,26 @@
-# Write your code here
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+
+class MovieDetailResponseSchema(BaseModel):
+    id: int
+    name: str
+    date: str
+    score: int
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: int
+    revenue: int
+    country: str
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieDetailResponseSchema]
+    prev_page: Optional[str]
+    next_page: Optional[str]
+    total_pages: int
+    total_items: int
